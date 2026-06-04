94257df438a0eb204fb091a8dcbf16c327d0a83e
Shivasai_95 <51985266+Shivasaii95@users.noreply.github.com>
2026-06-04 18:52:26 +0530

    Implemented GetAllPatients endpoint with empty result handling

---
+++ HealthcareManagementAPI/HealthcareManagementAPI.Models/DTOs/Patient/CreatePatientRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using HealthcareManagementAPI.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareManagementAPI.Models.DTOs.Patient
+{
+    public class CreatePatientRequestDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
